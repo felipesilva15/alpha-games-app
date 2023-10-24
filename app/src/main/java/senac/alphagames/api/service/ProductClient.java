0b519faba985e1575f4c8754aad1dca276dbefae
Felipe Silva <felipe.allware@gmail.com>
2023-10-23 21:18:42 -0300

    Fix: Adicionando o filtro de busca no client de produtos e rota de busca de todos

--- app/src/main/java/senac/alphagames/api/service/ProductClient.java
+++ app/src/main/java/senac/alphagames/api/service/ProductClient.java
@@ -5,11 +5,12 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import senac.alphagames.model.Product;
 
 public interface ProductClient {
     @GET("product")
-    Call<List<Product>> getProducts();
+    Call<List<Product>> getProducts(@Query("search") String search);
 
     @GET("product/{id}")
     Call<Product> getProductById(@Path("id") int id);
