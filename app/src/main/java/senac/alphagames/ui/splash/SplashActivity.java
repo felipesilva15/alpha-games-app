a538763af8db946054166e099fb7f4e8b078fb15
Felipe Silva <felipe.allware@gmail.com>
2023-10-30 20:46:48 -0300

    Feat: Criando um splash para o aplicativo

---
+++ app/src/main/java/senac/alphagames/ui/splash/SplashActivity.java
@@ -0,0 +1,30 @@
+package senac.alphagames.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import senac.alphagames.R;
+import senac.alphagames.ui.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
